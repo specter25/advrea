c486bbc5d89bd6715a616058e0dd147058c0015e
specter25 <ujjwalcoding012@gmail.com>
2020-08-02 13:48:57 +0530

    context provider accessed in components

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
  class Booklist extends Component {
+    static contextType = ThemeContext;
     render() {
+        console.log(this.context)
+        const {isLightTheme ,light , dark} = this.context;
+        const theme = isLightTheme?light:dark ;
         return (
-            <div className="book-list">
+            <div className="book-list" style ={{background:theme.bg, color:theme.syntax}}>
                 <ul>
-                    <li>The Kill List</li>
-                    <li>The fault in our stars</li>
-                    <li>the Day of  the Jackal</li>
+                    <li  style ={{background:theme.ui, }}>The Kill List</li>
+                    <li  style ={{background:theme.ui, }}>The fault in our stars</li>
+                    <li  style ={{background:theme.ui, }}>the Day of  the Jackal</li>
                 </ul>   
             </div>
         )
