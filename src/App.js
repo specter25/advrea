df0ee9a0469ad284cfc7cac698720910b89f57c0
specter25 <ujjwalcoding012@gmail.com>
2020-08-02 14:09:22 +0530

    changing context state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Booklist from './components/Booklist'
 import ThemeContextProvider from "./context/ThemeContext";
+import ThemeToggle from './components/ThemeToggle'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <Booklist />
+        <ThemeToggle />
 
       </ThemeContextProvider>
           </div>
---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+
+ class ThemeToggle extends Component {
+     static contextType = ThemeContext;
+     
+    render() {
+        const {toggleTheme} = this.context;
+        return (
+            <div>
+                <button className="btn-dark" onClick={toggleTheme}>Toggle Theme</button>
+            </div>
+        )
+    }
+}
+
+
+export default ThemeToggle
