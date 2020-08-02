2cd6bf80bf7047492a55e2e956223b33ddea3252
specter25 <ujjwalcoding012@gmail.com>
2020-08-02 12:42:22 +0530

    initial commit

---
+++ src/components/Booklist.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+ class Booklist extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>The Kill List</li>
+                    <li>The fault in our stars</li>
+                    <li>the Day of  the Jackal</li>
+                </ul>   
+            </div>
+        )
+    }
+}
+export default Booklist
